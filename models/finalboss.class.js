802b7282daa6f0b58719b7403bb706323fccb8df
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-06-30 17:07:40 +0200

    change boxx hurt

--- models/finalboss.class.js
+++ models/finalboss.class.js
@@ -33,8 +33,6 @@ class Finalboss extends MovableObject {
     imagesFinalbossDead = [
         'img/4_enemie_boss_chicken/5_dead/G24.png',
         'img/4_enemie_boss_chicken/5_dead/G25.png',
-        'img/4_enemie_boss_chicken/5_dead/G24.png',
-        'img/4_enemie_boss_chicken/5_dead/G25.png',
         'img/4_enemie_boss_chicken/5_dead/G26.png'
     ];
 
